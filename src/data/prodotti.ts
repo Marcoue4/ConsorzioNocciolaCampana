7d7833249480dcba11f4d0ed17eee334ca1b0d58
Marcoue4 <79284608+Marcoue4@users.noreply.github.com>
2026-02-27 15:28:56 +0100

    fixed images not displaying

--- src/data/prodotti.ts
+++ src/data/prodotti.ts
@@ -27,7 +27,7 @@ export const products: Product[] = [
   {
     title: 'Nocciole Campane',
     description: 'Nocciole Campane dal gusto unico',
-    image: '/images/AdobeStock_316441870-1-960x720.jpeg',
+    image: '/images/AdobeStock_142831303-kWaH--1020x533@IlSole24Ore-Web.avif',
     category: 'Nocciole',
     price: '€12.90',
     unit: '/ 500g',
@@ -36,7 +36,7 @@ export const products: Product[] = [
   {
     title: 'Noci Campane DOP',
     description: 'Noci Campane DOP, dolci e delicate',
-    image: '/images/7B93C52C-8AA1-471F-909A-A054FB2677EF-1-960x720.jpg',
+    image: '/images/jcr_content.jpg',
     category: 'Noci',
     price: '€14.50',
     unit: '/ 500g',
@@ -44,7 +44,7 @@ export const products: Product[] = [
   },
   {
     title: 'Crema di Nocciole Artigianale',
-    description: '70% nocciole, zero compromessi',
+    description: '45% nocciole, zero compromessi',
     image: '/images/Foto-1-Crema-Spamabile-Nocciola--960x720.jpg',
     category: 'Trasformati',
     price: '€9.90',
@@ -69,17 +69,17 @@ export const products: Product[] = [
     unit: '/ 300g',
   },
   {
-    title: 'Gheriglio di Noce Sgusciato',
-    description: "Noci sgusciate pronte all'uso",
-    image: '/images/filiere-frutta-in-guscio-800x445-1.jpg',
-    category: 'Noci',
+    title: 'Granella di Nocciole',
+    description: "Granella di nocciole pronte all'uso",
+    image: '/images/Granella-nocciole-tostate-3.jpg',
+    category: 'Nocciole',
     price: '€16.90',
     unit: '/ 400g',
   },
   {
     title: 'Olio di Nocciola Extra',
     description: 'Olio pregiato spremuto a freddo',
-    image: '/images/Facolta-Agraria-960x533.jpg',
+    image: '/images/Olio-di-nocciola.jpg',
     category: 'Trasformati',
     price: '€18.00',
     unit: '/ 250ml',
@@ -87,7 +87,7 @@ export const products: Product[] = [
   {
     title: 'Farina di Nocciole',
     description: "Per dolci e pasticceria d'eccellenza",
-    image: '/images/torrone-in-festa.jpg',
+    image: '/images/IMG_2282.jpg',
     category: 'Trasformati',
     price: '€11.50',
     unit: '/ 500g',
